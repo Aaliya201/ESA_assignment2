f5f1119ced82ea35f65e1efa980f4e9a60872885
Aaliya201 <55096773+Aaliya201@users.noreply.github.com>
2021-04-16 00:41:31 +0530

    Update userRoutes.js

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -4,15 +4,15 @@
     module.exports = function(app) {
         var userlist = require('../controllers/userController');
 
-// todoList Routes
+// User Routes
 
-// get and post request for /todos endpoints
+// get and post request for Users
         app
         .route("/rest/v1/users")
         .get(userlist.listofallusers)
         .post(userlist.createNewuser);
     
-//  put and delete request for Users endpoints
+//  put and delete request for Users
         app
         .route("/rest/v1/users/:username")
         .put(userlist.updateUser)
