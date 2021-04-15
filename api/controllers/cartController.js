3877d5c1c5df252410b42635812938bec47a4e38
Aaliya201 <55096773+Aaliya201@users.noreply.github.com>
2021-04-16 00:34:29 +0530

    Update cartController.js

--- api/controllers/cartController.js
+++ api/controllers/cartController.js
@@ -3,7 +3,7 @@ const  Cart = require("../models/cartModel");
 
 // DEFINE CONTROLLER FUNCTIONS
 
-// listofallitems function - To list all users
+// listofallitems function - To list all items
 exports.listofallitems = (req, res) => {
 Cart.find({}, (err, cart) => {
 if (err) {
@@ -14,7 +14,7 @@ res.status(200).json(cart);
 };
 
 
-// createNewcart function - To create new user
+// createNewcart function - To create new Cart
 exports.addmoreitems = (req, res) => {
 let  newCart = new Cart (req.body);
 newCart.save((err, cart) => {
@@ -25,7 +25,7 @@ res.status(201).json(cart);
 });
 };
 
- //updateCart function - To update user status by id
+ //updateCart function - To update Cart status by username
 exports.updateCart = (req, res) => {
 Cart.findOneAndUpdate({ username:req.params.username,productid:req.params.productid, }, req.body, { new:true }, (err, cart) => {
 if (err) {
@@ -35,7 +35,7 @@ res.status(200).json(cart);
 });
 };
 
-// deleteCart function - To delete user by userid
+// deleteCart function - To delete Cart by username
 exports.deleteCart = async ( req, res) => {
 await  Cart.deleteOne({ username:req.params.username,productid:req.params.productid, }, (err) => {
 if (err) {
