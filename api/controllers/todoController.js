1490019f5ebfe8e4576bde5b239b989ea3875161
Aaliya201 <55096773+Aaliya201@users.noreply.github.com>
2021-04-16 00:36:06 +0530

    Update todoController.js

--- api/controllers/todoController.js
+++ api/controllers/todoController.js
@@ -3,7 +3,7 @@ const  Todo = require("../models/todoModel");
 
 // DEFINE CONTROLLER FUNCTIONS
 
-// listAllTodos function - To list all todos(GET)
+// listAllTodos function - To list all products(GET)
 exports.listAllTodos = (req, res) => {
 Todo.find({}, (err, todo) => {
 if (err) {
@@ -14,7 +14,7 @@ res.status(200).json(todo);
 };
 
 
-// createNewTodo function - To create new todo(post)
+// createNewTodo function - To create new Product(post)
 exports.createNewTodo = (req, res) => {
 let  newTodo = new Todo (req.body);
 newTodo.save((err, todo) => {
@@ -25,7 +25,7 @@ res.status(201).json(todo);
 });
 };
 
-// updateTodo function - To update todo status by id
+// updateTodo function - To update Products by productid
 exports.updateTodo = (req, res) => {
 Todo.findOneAndUpdate({ productid:req.params.productid }, req.body, { new:true }, (err, todo) => {
 if (err) {
@@ -35,7 +35,7 @@ res.status(200).json(todo);
 });
 };
 
-// deleteTodo function - To delete todo by id
+// deleteTodo function - To delete product by productid
 exports.deleteTodo = async ( req, res) => {
 await  Todo.deleteOne({ productid:req.params.productid }, (err) => {
 if (err) {
